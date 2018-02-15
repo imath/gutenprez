ff3dcc065c54fc9c70786bc88777478ff4caedc4
imath <math.viet@gmail.com>
2018-02-15 22:18:07 +0100

    Improve slides and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,38 @@
-const path = require( 'path' );
 const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+
+// CSS loader for styles specific to block editing.
+const blocksCSS = new ExtractTextPlugin( {
+	filename: './assets/blocks.min.css',
+} );
 
 module.exports = {
-	entry: './js/navigation.js',
+	entry: [
+		'./js/navigation.js',
+		'./assets/blocks.css',
+	],
 	output: {
-		filename: 'navigation.min.js',
-		path: path.resolve( __dirname, 'js' ),
+		filename: './js/navigation.min.js',
+		path: __dirname,
+	},
+	module: {
+		rules: [ {
+			test: /\.css$/,
+			use: blocksCSS.extract( {
+				fallback: 'style-loader',
+				use: {
+					loader: 'css-loader',
+					options: {
+						url: false,
+						minimize: true,
+						sourceMap: true,
+					},
+				},
+			} ),
+		} ],
 	},
 	plugins: [
+		blocksCSS,
 		new UglifyJsPlugin(),
 	],
 };
